b5850322fb160424280e2891b82ee9a7a2f59259
WolfY <david.skliarov.dev@gmail.com>
2026-06-22 16:50:54 +0400

    feat: add error-handler middleware

---
+++ backend/src/middleware/error-handler.ts
@@ -0,0 +1,11 @@
+import type { NextFunction, Request, Response } from "express";
+
+export function errorHandler(
+	err: Error,
+	_req: Request,
+	res: Response,
+	_next: NextFunction,
+) {
+	console.error(err.stack);
+	res.status(500).json({ message: "Internal server error" });
+}
